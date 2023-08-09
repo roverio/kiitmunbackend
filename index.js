1d825baf03517cf1eb35a4fee51b5e9232b21b3d
Revenant <dasdev.pratik@gmail.com>
2023-08-09 17:47:00 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('./app')
 
-app.listen(4000, () => {
- console.log(`Server running on port 4000`)
+
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+ console.log(`Server running on port ${PORT}`)
 })
